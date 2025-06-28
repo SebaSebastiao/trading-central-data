3dcdbe3124a300026159a1bea7da3438c5b7c27d
SebaSebastiao <seba.sebastian7@hotmail.com>
2025-06-28 13:16:28 -0300

    Atualizar para PWA - Progressive Web App

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'trading-central-v1';
+const urlsToCache = [
+  '/trading-central-data/',
+  '/trading-central-data/index.html',
+  '/trading-central-data/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
